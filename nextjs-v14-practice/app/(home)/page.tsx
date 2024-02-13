b30a473808e671337710e15ca2a5fee4f7d7e3f2
mintcoo <xhaktm3344@gmail.com>
2024-02-13 17:31:00 +0900

    metadata 설정 및 group route

--- nextjs-v14-practice/app/(home)/page.tsx
+++ nextjs-v14-practice/app/(home)/page.tsx
@@ -1,9 +1,12 @@
-import Navigation from "../components/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Hello</h1>
     </div>
   );
